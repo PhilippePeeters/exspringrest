68fb6838892c6d8ba50285ba5a6d54d0944dc5f9
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-10-12 15:29:43 +0200

    ExA1 Rest API PersonContreller

---
+++ ExA1/src/main/java/be/abis/exercice/ExA1Application.java
@@ -0,0 +1,13 @@
+package be.abis.exercice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExA1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExA1Application.class, args);
+	}
+
+}
