6e5d4fa9baaf821dbf60245041bab3c311626791
Philippe Peeters <philippe.a.peeters@bnpparibasfortis.com>
2020-10-13 08:31:49 +0200

    ExA2 Rest API

--- ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
+++ ExA2/src/main/java/be/abis/exercise/controller/PersonController.java
@@ -1,10 +1,9 @@
 package be.abis.exercise.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.abis.exercise.exception.PersonCanNotBeDeletedException;
-import be.abis.exercise.model.LoginItem;
 import be.abis.exercise.model.Person;
 import be.abis.exercise.service.PersonService;
 
@@ -24,20 +22,15 @@ public class PersonController {
 	@Autowired
 	PersonService personService;
 	
-	@GetMapping("/persons/")
-	public ArrayList<Person> getAllPersons() {
-		return personService.getAllPersons();
-		
-	}
-
+	
 	@GetMapping("/persons/{id}")
 	public Person findPerson(@PathVariable("id") int id) {
 		return personService.findPerson(id);
 	}
 
-	@GetMapping("/persons/{email}/{passWord}")
-	public Person findPerson(@PathVariable("email") String emailAddress, @PathVariable("passWord") String passWord) {
-		return personService.findPerson(emailAddress, passWord);
+	@GetMapping("/persons/")
+	public List<Person> getAllPersons() {
+		return personService.getAllPersons();
 		
 	}
 	
@@ -61,7 +54,13 @@ public class PersonController {
 		}
 		
 	}
-	
+
+	@GetMapping("/persons/{email}/{passWord}")
+	public Person findPerson(@PathVariable("email") String emailAddress, @PathVariable("passWord") String passWord) {
+		return personService.findPerson(emailAddress, passWord);
+		
+	}
+
 	@PutMapping("/password/{person}/{passWord}")
 	public void changePassword(@RequestBody Person person, @PathVariable("passWord") String newPswd) throws IOException {
 		personService.changePassword(person, newPswd);
